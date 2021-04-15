c38097e88ebc55fb66ac67071ceede98655f3a96
KonWys01 <konradwysokinski01@gmail.com>
2021-04-16 00:16:02 +0200

    method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from fastapi import Request
+import time
 app = FastAPI()
 app.counter = 0
 
@@ -19,3 +20,38 @@ def counter():
     app.counter += 1
     return app.counter
 
+
+"""
+@app.get("/method")
+def method():
+    return {"method": "GET"}
+
+
+@app.post("/method", status_code=201)
+def method():
+    return {"method": "POST"}
+
+
+@app.delete("/method")
+def method():
+    return {"method": "DELETE"}
+
+
+@app.put("/method")
+def method():
+    return {"method": "PUT"}
+
+
+@app.options("/method")
+def method():
+    return {"method": "OPTIONS"}"""
+
+
+@app.api_route("/method", methods=["GET", 'DELETE', 'PUT', 'OPTIONS'])
+def method(request: Request):
+    return {"method": request.method}
+
+
+@app.post("/method", status_code=201)
+def method(request: Request):
+    return {"method": request.method}
