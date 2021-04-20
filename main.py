a50cfc5b5f5dcebb5fe14387cfc714d93ae9ee53
KonWys01 <konradwysokinski01@gmail.com>
2021-04-20 02:17:16 +0200

    len of password_hash

--- main.py
+++ main.py
@@ -69,12 +69,11 @@ def method(request: Request):
 
 
 @app.get("/auth")
-def auth(response: Response, password: str, password_hash: str):
+def auth(password: str, password_hash: str, response: Response):
 
-    if len(password) == 0:
+    if len(password) == 0 or len(password_hash) == 0:
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return response.status_code
-
     hashed = hashlib.sha512(password.encode())
     if hashed.hexdigest() == password_hash:
         response.status_code = status.HTTP_204_NO_CONTENT
