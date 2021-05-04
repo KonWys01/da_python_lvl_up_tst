633eff1ad108f5863059a1755fe0f546c1b9bff9
KonWys01 <konradwysokinski01@gmail.com>
2021-05-04 18:56:44 +0200

    plain text in another function

--- main.py
+++ main.py
@@ -338,6 +338,11 @@ def read_current_user(resposne: Response, credentials: HTTPBasicCredentials = De
         return {"token": token_value}
 
 
+@app.get(response_class=PlainTextResponse)
+def plain_text():
+    return "Welcome!"
+
+
 @app.get("/welcome_session")
 def welcome_session(*, response: Response, session_token: str = Cookie(None), format: str = ""):
     if session_token not in app.login_session_tokens:
@@ -346,8 +351,7 @@ def welcome_session(*, response: Response, session_token: str = Cookie(None), fo
     else:
         response.status_code = status.HTTP_200_OK
         if format == "":
-            result = "Welcome"
-            return result
+            return plain_text()
         elif format == "json":
             return {"message": "Welcome!"}
         elif format == "html":
@@ -373,8 +377,7 @@ def welcome_token(response: Response, token: str = "", format: str = ""):
     else:
         response.status_code = status.HTTP_200_OK
         if format == "":
-            result = "Welcome"
-            return result
+            return plain_text()
         elif format == "json":
             return {"message": "Welcome!"}
         elif format == "html":
