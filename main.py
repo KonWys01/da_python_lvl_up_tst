9157867f4853f45cf156f1730f33e41809724a9c
KonWys01 <konradwysokinski01@gmail.com>
2021-05-11 21:58:36 +0200

    4.6 bring back await

--- main.py
+++ main.py
@@ -690,19 +690,17 @@ async def categories_6(response: Response, id: int):
         """
         SELECT EXISTS(SELECT 1 FROM Categories WHERE CategoryID=:id) as if_exist
         """, {'id': id})
-    data = cursor.fetchall()
+    data = await cursor.fetchall()
     if_exist = data[0]['if_exist']
     if if_exist == 1:
-        response.status_code = status.HTTP_200_OK
-        cursor = app.db_connection.execute(
+        cursor = await app.db_connection.execute(
             """
             delete from Categories
             where CategoryID =:id
             """, {'id': id})
-        app.db_connection.commit()
+        data = await cursor.fetchall()
         return {"deleted": 1}
     else:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return response
+        raise HTTPException(status_code=404)
 
 
